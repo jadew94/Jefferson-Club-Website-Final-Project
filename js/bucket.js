5e74fb9debae990874083f69c642b57411406204
Jade <jadewilburn@Jades-MacBook-Air.local>
2018-11-29 15:42:50 -0500

    Added functionality to select an event category and instructions

--- js/bucket.js
+++ js/bucket.js
@@ -17,6 +17,10 @@ $( document ).ready(function() {
      $( "#datepicker" ).datepicker();
    } );
 
+   $(".dropdown-menu li a").click(function(){
+     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
 
+   });
 
 });
